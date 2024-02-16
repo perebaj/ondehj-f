64899807423abd92285f5cdec5dc7f4e10994379
jojo <perebaj@gmail.com>
2024-02-16 08:25:20 -0300

    :sparkles: add shadcnui & tailwind & eslint & pretier

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
-function App() {
-  return <div>Teste</div>
-}
+import './globals.css'
 
-export default App
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+
+import { Button } from '@/components/ui/button'
+export default function Home() {
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | ondehoje?" />
+
+      <div className="flex h-screen items-center justify-center">
+        <Button>Enviar</Button>
+      </div>
+    </HelmetProvider>
+  )
+}
