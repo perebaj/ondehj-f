75032a4b6751798e3d856e600c9c518b3b5e946a
jojo <perebaj@gmail.com>
2024-02-21 12:02:31 -0300

    :sparkles: add dropdown dialog and minors

--- src/billing.tsx
+++ src/billing.tsx
@@ -1,58 +1,50 @@
+import { Copy } from 'lucide-react'
 import { QRCodeSVG } from 'qrcode.react'
 
 import { Button } from '@/components/ui/button'
 import {
-  Dialog,
+  // Dialog,
   DialogContent,
   DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from '@/components/ui/dialog'
-
-// import DashboardHeader from './dashboard-header'
 
 export default function Billing() {
   return (
-    <div className="font-Fredoka">
-      <Dialog>
-        <DialogTrigger>Assinatura</DialogTrigger>
-        <DialogContent className="max-w-xs lg:max-w-2xl">
-          <DialogHeader>
-            <div className="flex flex-col items-center justify-center gap-2 px-4 py-4">
-              <DialogTitle className="text-2xl text-blue-500">
-                Código Gerado com sucesso!
-              </DialogTitle>
-              <DialogDescription>
-                O pagamento deve ser realizado até às 23:59 do dia 10/10/2021
-                antes que o código expire.
-              </DialogDescription>
-            </div>
-            <div className="flex flex-col items-center justify-center gap-6 ">
-              <p className="flex flex-col items-center justify-center py-2 text-3xl">
-                <span className=" font-extrabold  text-green-500">
-                  R$ 30,00
-                </span>{' '}
-                <span className="">
-                  Acesso <span className="font-extrabold">ilimitado</span>{' '}
-                  <span>o ano todo!</span>
-                </span>
-              </p>
-              <QRCodeSVG className="h-64 w-64" value="https://reactjs.org/" />
-              <div className="pt-8">
-                <Button
-                  variant={'outline'}
-                  className="bg-green-500 text-white hover:bg-green-600"
-                >
-                  Copiar código
-                </Button>
-              </div>
-            </div>
-          </DialogHeader>
-          <DialogFooter></DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </div>
+    <DialogContent className="font-Fredoka max-w-xs lg:max-w-2xl">
+      <DialogHeader>
+        <div className="flex flex-col items-center justify-center gap-2 px-4 py-4">
+          <DialogTitle className="text-2xl text-blue-500">
+            Código Gerado com sucesso!
+          </DialogTitle>
+          <DialogDescription>
+            O pagamento deve ser realizado até às 23:59 do dia 10/10/2021 antes
+            que o código expire.
+          </DialogDescription>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-6 ">
+          <p className="flex flex-col items-center justify-center py-2 text-3xl">
+            <span className=" font-extrabold  text-green-500">R$ 30,00</span>{' '}
+            <span className="">
+              Acesso <span className="font-extrabold">ilimitado</span>{' '}
+              <span>o ano todo!</span>
+            </span>
+          </p>
+          <QRCodeSVG className="h-64 w-64" value="https://reactjs.org/" />
+          <div className="pt-8">
+            <Button
+              variant={'outline'}
+              className="gap-2 bg-green-500 text-white hover:bg-green-600"
+            >
+              Copiar código
+              <Copy strokeWidth={2} size={18} />
+            </Button>
+          </div>
+        </div>
+      </DialogHeader>
+      <DialogFooter></DialogFooter>
+    </DialogContent>
   )
 }
--- src/dashboard-header.tsx
+++ src/dashboard-header.tsx
@@ -1,6 +1,7 @@
 import { ChevronDown, CreditCard } from 'lucide-react'
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Dialog, DialogTrigger } from '@/components/ui/dialog'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -17,35 +18,40 @@ export default function DashboardHeader() {
     <div className="font-Fredoka">
       <header className="border-b bg-slate-50">
         <div className="mx-auto flex items-center justify-center gap-4 py-3 max-lg:px-4 lg:max-w-7xl lg:justify-between">
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <div className="mr-auto">
-                <Button variant={'outline'} className="gap-2">
-                  <Avatar className="h-7 w-7">
-                    <AvatarImage src="https://github.com/shadcn.png" />
-                    <AvatarFallback>CN</AvatarFallback>
-                  </Avatar>
-                  Jonathan S
-                  <ChevronDown strokeWidth={1} />
-                </Button>
-              </div>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>
-                <p className="flex flex-col items-center text-slate-500">
-                  <span>Minha Conta</span>
-                  <span>perebaj@gmail.com</span>
-                </p>
-              </DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem className="no-underline hover:underline">
-                Perfil
-              </DropdownMenuItem>
-              <DropdownMenuItem className="no-underline hover:underline">
-                <Billing />
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <Dialog>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <div className="mr-auto">
+                  <Button variant={'outline'} className="gap-2">
+                    <Avatar className="h-7 w-7">
+                      <AvatarImage src="https://github.com/shadcn.png" />
+                      <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    Jonathan S
+                    <ChevronDown strokeWidth={1} />
+                  </Button>
+                </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>
+                  <p className="flex flex-col items-center text-slate-500">
+                    <span>Minha Conta</span>
+                    <span>perebaj@gmail.com</span>
+                  </p>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className="no-underline hover:underline">
+                  Perfil
+                </DropdownMenuItem>
+                <DialogTrigger>
+                  <DropdownMenuItem className=" no-underline hover:underline">
+                    Assinatura
+                  </DropdownMenuItem>
+                </DialogTrigger>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <Billing />
+          </Dialog>
           <a
             href="/"
             className="hidden lg:flex lg:items-center lg:justify-center lg:gap-2"
@@ -55,7 +61,10 @@ export default function DashboardHeader() {
           <div className="hidden lg:flex lg:items-center lg:justify-center">
             <Button className="gap-2 bg-primary">
               <CreditCard strokeWidth={1} size={18} />
-              <Billing />
+              <Dialog>
+                <DialogTrigger>Assinatura</DialogTrigger>
+                <Billing />
+              </Dialog>
             </Button>
           </div>
         </div>
