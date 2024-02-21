9a541c960fc47ff0a4f88f6988d35f5f2435279b
jojo <perebaj@gmail.com>
2024-02-21 15:55:31 -0300

    :bug: chore: footer

--- src/events.tsx
+++ src/events.tsx
@@ -9,7 +9,7 @@ import Event2 from './components/event'
 export default function Events() {
   return (
     <div className="w-full py-10 lg:py-14">
-      <div className="container grid items-center gap-4 px-4 text-center md:px-6">
+      <div className="container grid items-center gap-4 px-4 py-4 text-center md:px-6 md:py-6">
         <div className="space-y-3">
           <h1 className="text-3xl font-bold tracking-tighter text-primary sm:text-4xl md:text-5xl">
             Próximos Eventos
@@ -19,8 +19,10 @@ export default function Events() {
           </p>
         </div>
       </div>
-      <div className="container grid max-w-6xl justify-center gap-4 px-4 md:px-6">
+      <div className="container grid max-w-6xl justify-center gap-4 px-4 py-4 md:px-6 md:py-6">
         <div className="grid grid-cols-1 items-stretch justify-center gap-4 md:grid-cols-2">
+          <Event2 />
+          <Event2 />
           <Event2 />
           <Event2 />
           <Event2 />
