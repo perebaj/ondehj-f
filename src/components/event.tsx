3f324f38621d177babc84c58e276f15bd006ee3b
Rafael Santos <rafaelsaannttos@gmail.com>
2024-02-24 15:36:04 -0300

    fix heigth of itens, not ready yet

--- src/components/event.tsx
+++ src/components/event.tsx
@@ -9,6 +9,8 @@ import {
 } from 'lucide-react'
 import moment from 'moment'
 import { useState } from 'react'
+import { TbArrowBadgeUpFilled } from "react-icons/tb";
+
 type EventProps = {
     name: string
     description: string
@@ -21,7 +23,8 @@ export default function Event(props: EventProps) {
     moment.locale('pt-br')
     const date = moment(props.date).format('LLL')
     const [isOpen, setIsOpen] = useState<boolean>(false)
-
+    const [isPromoted, setIsPromoted] = useState<boolean>(false)
+    
     let TypeIcon
     let TypeName
     switch (props.type) {
@@ -52,8 +55,14 @@ export default function Event(props: EventProps) {
 
     return (
         <div
-            onClick={() => toggleItem()}
-            className={`flex flex-col items-start cursor-pointer justify-start gap-1 rounded-lg border p-6 shadow-md overflow-hidden h-full transition-max-height duration-300 relative min-h-[256px] ease-in-out ${isOpen ? 'max-h-[1300px]' : 'h-full max-h-60'}`}>
+            className={`flex flex-col items-start justify-start gap-1 rounded-lg border p-6 shadow-md overflow-hidden h-full transition-max-height duration-300 relative min-h-[256px] ease-in-out ${isOpen ? 'max-h-[1300px]' : 'h-full max-h-60'}`}>
+            <button
+                typ="button"
+                onClick={() => { setIsPromoted(!isPromoted) }}
+                className={`${isPromoted ? "text-gray-100 bg-green-700" : "bg-gray-100 text-green-700"} absolute transition-all top-5 border right-4 rounded-full px-2 flex font-semibold  items-center gap-1 border-gray-200  `} >
+                <TbArrowBadgeUpFilled></TbArrowBadgeUpFilled>
+                Up!
+            </button>
             <span className="inline-flex items-center rounded-full border border-gray-200 bg-gray-100 px-2.5 py-1 text-xs font-semibold text-gray-900 dark:border-gray-800 dark:bg-gray-950 dark:text-gray-50">
                 <TypeIcon className="mr-1.5 h-4 w-4" />
                 {TypeName}
@@ -69,17 +78,24 @@ export default function Event(props: EventProps) {
             </div>
 
 
-            {!isOpen &&
+            {!isOpen ?
                 <>
-
-
                     <div className="absolute bottom-0 left-0 right-0 w-full h-[30%] bg-gradient-to-t from-white">
-
                     </div>
-                        <span className="absolute right-4 bottom-4 bg-primary px-3 py-1 text-white font-semibold rounded-md" >
-                            Ver mais
-                        </span>
+                    <button
+                        type='button'
+                        onClick={() => toggleItem()}
+                        className="absolute right-4 bottom-4 bg-primary px-3 py-1 text-white font-semibold rounded-md" >
+                        Ver mais
+                    </button>
                 </>
+                :
+                <button
+                    type='button'
+                    onClick={() => toggleItem()}
+                    className="absolute right-4 bottom-4 bg-primary px-3 py-1 text-white font-semibold rounded-md" >
+                    Ver menos
+                </button>
 
             }
 
